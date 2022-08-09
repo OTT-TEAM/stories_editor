16403205f7f13eb0ed33b9d3f0bc4d4b7f943671
rivolt2022 <rivolt2022@gmail.com>
2022-08-09 16:34:28 +0900

    add comment

--- lib/src/presentation/widgets/animated_onTap_button.dart
+++ lib/src/presentation/widgets/animated_onTap_button.dart
@@ -20,6 +20,7 @@ class AnimatedOnTapButton extends StatefulWidget {
 
 class _AnimatedOnTapButtonState extends State<AnimatedOnTapButton>
     with TickerProviderStateMixin {
+  //확장과 축소
   double squareScaleA = 1;
   AnimationController? _controllerA;
   Timer _timer = Timer(const Duration(milliseconds: 300), () {});
@@ -29,9 +30,12 @@ class _AnimatedOnTapButtonState extends State<AnimatedOnTapButton>
     if (mounted) {
       _controllerA = AnimationController(
         vsync: this,
+        //애니메이션이 얻을 수 있는 최소값, 애니메이션이 해제된 것으로 간주되는 값
         lowerBound: 0.95,
         upperBound: 1.0,
+        //애니메이션의 시작값
         value: 1,
+        //애니메이션이 지속되는 시간
         duration: const Duration(milliseconds: 10),
       );
       _controllerA?.addListener(() {
@@ -55,6 +59,7 @@ class _AnimatedOnTapButtonState extends State<AnimatedOnTapButton>
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      //경계 내에서 이벤트를 수신하고 시각적으로 뒤에 있는 타겟도 이벤트를 수신
       behavior: HitTestBehavior.translucent,
       onTap: () {
         /// set vibration
@@ -69,6 +74,9 @@ class _AnimatedOnTapButtonState extends State<AnimatedOnTapButton>
         try {
           if (mounted) {
             _timer = Timer(const Duration(milliseconds: 100), () {
+              //스프링처럼 튀는 애니메이션
+              //스프링 및 초기 속도로 애니메이션을 구동
+              //속도가 양수이면 애니메이션이 완료되고 그렇지 않으면 종료
               _controllerA!.fling();
             });
           }
